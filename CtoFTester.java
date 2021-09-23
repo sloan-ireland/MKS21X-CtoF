b271f9123d9de8f11e6577b88055381532011d04
Sloan Ireland <sireland30@stuy.edu>
2021-09-23 19:51:15 -0400

    made working main and helper functions that ouput

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,13 @@
-holder
+public class CtoFTester{
+  public static void celsiusToFahrenheit(){
+    System.out.print("working code");
+  }
+
+  public static void fahrenheitToCelsius(){
+    System.out.print("working code #2");
+  }
+
+  public static void main(String[] args){
+    System.out.print("working code #3");
+  }
+}
