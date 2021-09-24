6cfac17ca5781d522a6c7ca69e99247a49970a20
Sloan Ireland <sireland30@stuy.edu>
2021-09-23 20:29:57 -0400

    Added body to the functions, test cases and the comments with answers to questions

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,13 +1,33 @@
+/* Questions from HW 06:
+
+What should the functions have as parameter?
+Each method to convert should have one parameter which is the input Temperature to
+ be converted to the other.
+the main should have the normal (String[] args)
+
+What type should they be?
+They should be double so no rounding to zero occurs when working with the intergers
+and the fraction in the formula.
+
+What should the functions return?
+the converted temperature from the input
+*/
 public class CtoFTester{
-  public static void celsiusToFahrenheit(){
-    System.out.print("working code");
+  public static double celsiusToFahrenheit(double celcius){
+    return (celcius * (9/5) + 32);
   }
 
-  public static void fahrenheitToCelsius(){
-    System.out.print("working code #2");
+  public static double fahrenheitToCelsius(double farenheight){
+    return ((farenheight - 32) * (5/9));
   }
 
   public static void main(String[] args){
-    System.out.print("working code #3");
+    System.out.println("Celsius to farenheight with example input of 31.");
+    System.out.println("expected value 87.8");
+    System.out.println(celsiusToFahrenheit(31));
+    System.out.println("\n");
+    System.out.println("farenheight to celcius with example input of 96");
+    System.out.println("expected value 35.5556");
+    System.out.println(fahrenheitToCelsius(96));
   }
 }
